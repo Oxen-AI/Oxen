5ee9cf06e50738d84f2b0361c90f670e7c927179
Lilianxr <lilian.zhou26@outlook.com>
2025-10-21 16:21:33 -0700

    Use QUERY method for batch download

--- oxen-rust/src/lib/src/api/client/entries.rs
+++ oxen-rust/src/lib/src/api/client/entries.rs
@@ -18,6 +18,7 @@ use async_tar::Archive;
 use flate2::write::GzEncoder;
 use flate2::Compression;
 use futures_util::{StreamExt, TryStreamExt};
+use http::Method;
 use std::fs::{self};
 use std::io::prelude::*;
 use std::io::Cursor;
@@ -846,10 +847,15 @@ pub async fn try_download_data_from_version_paths(
     }
     let body = encoder.finish()?;
     log::debug!("download_data_from_version_paths body len: {}", body.len());
-    let url = api::endpoint::url_from_repo(remote_repo, "/versions/fetch")?;
-
+    let url = api::endpoint::url_from_repo(remote_repo, "/versions")?;
     let client = client::new_for_url(&url)?;
-    if let Ok(res) = client.post(&url).body(body).send().await {
+    let query_method = Method::from_bytes(b"QUERY").unwrap();
+    if let Ok(res) = client
+        .request(query_method.clone(), &url)
+        .body(body)
+        .send()
+        .await
+    {
         if reqwest::StatusCode::UNAUTHORIZED == res.status() {
             let err = "Err: unauthorized request to download data".to_string();
             log::error!("{err}");
