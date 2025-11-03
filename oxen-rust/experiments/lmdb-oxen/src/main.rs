8bb00365e98bf4c7c64f6b85a7eae1e61840a033
Surya <suriyaganesh097@gmail.com>
2025-11-03 10:11:07 -0500

    Oxb 251/lmdb (#152)
    * add lmdb
    
    * working migration with proper DB creation
    
    * fix format error

---
+++ oxen-rust/experiments/lmdb-oxen/src/main.rs
@@ -0,0 +1,45 @@
+use clap::{Parser, Subcommand};
+use std::{fs, string};
+use std::path::PathBuf;
+use std::time::{Duration, Instant, SystemTime, UNIX_EPOCH};
+
+use framework::{FrameworkError, FrameworkResult};
+
+pub mod framework;
+pub mod migrate;
+pub mod lmdb;
+
+struct TestMetrics {
+    pack_time: Duration,
+    unpack_time: Duration,
+    _pack_cpu_usage: f32,
+    _pack_memory_usage_bytes: u64,
+    _unpack_cpu_usage: f32,
+    _unpack_memory_usage_bytes: u64,
+}
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Migrate,
+    Test,
+}
+
+fn main() -> FrameworkResult<()> {
+    let args = Args::parse();
+
+    match args.command {
+        Commands::Migrate => {
+            migrate::migrate()
+        }
+        Commands::Test => {
+            migrate::test()
+        }
+    }
+}
