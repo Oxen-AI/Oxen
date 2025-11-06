f5948f2aaed5824c1cbec7aa20a56a426b66769e
Joshua Elliott <joshuacelliott@gmail.com>
2025-11-06 09:14:50 -0700

    Update to Rust 1.88
    - fix or ignore some large_enum_variant clippy warnings

--- oxen-rust/src/lib/src/model/diff/diff_result.rs
+++ oxen-rust/src/lib/src/model/diff/diff_result.rs
@@ -2,6 +2,8 @@
 use crate::model::diff::tabular_diff::TabularDiff;
 use crate::model::diff::text_diff::TextDiff;
 
+// ignoring because we likely won't have enough of these in memory to matter
+#[allow(clippy::large_enum_variant)]
 #[derive(Debug, Clone)]
 pub enum DiffResult {
     Tabular(TabularDiff),
