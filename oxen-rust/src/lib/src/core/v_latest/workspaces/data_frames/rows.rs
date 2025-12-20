9280e77bc4f13afc7478ad48cbcb17a9dac0f762
eloymartinez <eloyalbmartinez@gmail.com>
2025-12-19 22:29:22 -0400

    remove logs

--- oxen-rust/src/lib/src/core/v_latest/workspaces/data_frames/rows.rs
+++ oxen-rust/src/lib/src/core/v_latest/workspaces/data_frames/rows.rs
@@ -44,7 +44,6 @@ pub fn add(
     let mut result = with_df_db_manager(db_path, |manager| {
         manager.with_conn(|conn| rows::append_row(conn, &df))
     })?;
-    println!("this is the result {result:?}");
     let oxen_id_col = result
         .column("_oxen_id")
         .expect("Column _oxen_id not found");
@@ -161,7 +160,6 @@ pub fn update(
     let row_changes_path = repositories::workspaces::data_frames::row_changes_path(workspace, path);
 
     let mut df = tabular::parse_json_to_df(data)?;
-    log::debug!("update() df: {df:?}");
     let mut row = repositories::workspaces::data_frames::rows::get_by_id(workspace, path, row_id)?;
     if row.height() == 0 {
         return Err(OxenError::resource_not_found("row not found"));
