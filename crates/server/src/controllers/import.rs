e4400142686e8b82c92338ae2c67127d58cf7798
Nathan Stocks <nathan.s@oxen.ai>
2026-03-26 12:21:06 -0600

    Fix more tests' path usage (#379)
    - Remove outdated/useless `version_path` test. It was testing an
    implementation detail of local version storage, which isn't useful, and
    it looks like it was intended to test an old standalone function.
    - fix `test_controllers_file_import_tabular_file` and
    `test_controllers_file_import_text_file` to check for version file
    existence through the trait correctly

--- crates/server/src/controllers/import.rs
+++ crates/server/src/controllers/import.rs
@@ -525,10 +525,11 @@ mod tests {
         let entry =
             repositories::entries::get_file(&repo, &resp.commit, "data/cats_vs_dogs.tsv")?.unwrap();
         let version_store = repo.version_store()?;
-        let version_path = version_store
-            .get_version_path(&entry.hash().to_string())
-            .await?;
-        assert!(version_path.exists());
+        assert!(
+            version_store
+                .version_exists(&entry.hash().to_string())
+                .await?
+        );
 
         // cleanup
         test::cleanup_sync_dir(&sync_dir)?;
@@ -591,10 +592,11 @@ mod tests {
         )?
         .unwrap();
         let version_store = repo.version_store()?;
-        let version_path = version_store
-            .get_version_path(&entry.hash().to_string())
-            .await?;
-        assert!(version_path.exists());
+        assert!(
+            version_store
+                .version_exists(&entry.hash().to_string())
+                .await?
+        );
 
         // cleanup
         test::cleanup_sync_dir(&sync_dir)?;
