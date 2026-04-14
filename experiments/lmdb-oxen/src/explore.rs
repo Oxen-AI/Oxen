a15d59a22e9c9642325a3cfbf94cc701a6115109
Malcolm Greaves <malcolm.g@oxen.ai>
2026-04-13 17:33:41 -0700

    wip

--- experiments/lmdb-oxen/src/explore.rs
+++ experiments/lmdb-oxen/src/explore.rs
@@ -1,2 +1,3 @@
 pub mod new_main;
 pub mod scratch;
+pub mod migration_explore;
--- experiments/lmdb-oxen/src/explore/scratch.rs
+++ experiments/lmdb-oxen/src/explore/scratch.rs
@@ -1,3 +1,5 @@
+use std::{ops::Deref};
+
 use liboxen::error::OxenError;
 use thiserror::Error as ThisError;
 
@@ -11,80 +13,101 @@ pub enum Error {
     Oxen(#[from] OxenError),
 }
 
-fn main() -> Result<(), Error> {
-
-  // create new lmdb database
-
-  // ==== MIGRATION SCRIPT ====
-  // walk an oxen repo directory: .oxen/tree/nodes/
-  //  for each {hash_prefix}
-  //    for each {hash_suffix}
-  //      hash = {prefix} + {suffix}
-  //      look at ./{ha/sh}/nodes and ./{ha/sh}/children
-  //      create a new entry in nodes table: {hash} -> serialized contents of node file
-  //      create a new entry in children table: {hash} -> serialized contents of children file
-  //      create a new entry in dir_hashes table: {hash} -> []each child hash
+// ==== DESIGN ====
 
 
-  // ==== DESIGN ====
-
-
-  enum MerkleError {
+enum MerkleError {
     HashDoesNotExist,
     HashDoesNotEqualContent,
-  }
+}
 
-  #[derive(Debug, Error)]
-  #[error("{0} is not a valid hex-encoded u128 value")]
-  pub struct NotAValidHexHash<'a>(&'a str)
+#[derive(Debug, Clone)]
+pub struct Hash(u128);
 
-  pub struct Hash(u128);
-  impl Hash {
-    pub fn from_hex<'a>(hex_encoded_hash: &'a str) -> Result<Self, NotAValidHexHash<'a>>;
+#[derive(Debug, Clone)]
+pub struct HexHash(String);
 
-    pub fn to_hex(&self) -> String;
+#[derive(Debug, ThisError)]
+#[error("{0} is not a valid hex-encoded u128 value")]
+pub struct NotAValidHexHash<'a>(&'a str);
 
-    pub fn new(content: impl Iterator<Item=u8>) -> Self;
-  }
+impl <'a> TryFrom<&'a str> for Hash {
+    type Error = NotAValidHexHash<'a>;
 
-  pub trait MerkleTreePlatform {
-
-    trait Node {
-      fn hash(&self) -> Hash;
-      fn name(&self) -> &str;
+    fn try_from(value: &'a str) -> Result<Self, Self::Error> {
+        let hash = u128::from_str_radix(value, 16)
+            .map_err(|_| NotAValidHexHash(value))?;
+        Ok(Hash(hash))
     }
-    type N : Node;
+}
 
-    trait Store {}
-    type S : Store;
 
-  }
+impl From<Hash> for HexHash {
+    fn from(value: Hash) -> Self {
+        HexHash(format!("{:032x}", value.0))
+    }
+}
 
-  pub trait MerkleMetadataStore<'db> {
+/// Safe conversion since we can only create a `HexHash` from a `Hash`.
+impl From<&HexHash> for Hash {
+    fn from(value: &HexHash) -> Self {
+        value.0.try_from()
+            .expect("Invariant violated! A HexHash was made that bypassed safe creation!")
+    }
+}
 
-    /// If true, then there is a node in the Merkle tree that has this hash.
-    fn exists(&'db self, hash: Hash) -> bool;
+/// Writes as a hex-encoded string.
+impl std::fmt::Display for HexHash {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
 
-    /// Obtains a reference to the Merkle tree node for the given hash.
-    /// None means there is no node with that hash.
-    fn node(&'db self, hash: Hash) -> Option<&'db MerkleNode>;
+/// Allows a `HexHash` to be dereferenced as a `str`.
+impl Deref for HexHash {
+    type Target = str;
 
-    /// Ensures that `content` is a child of `parent`.
-    /// If content is already a child of parent, then this does not change the Merkle tree.
-    /// It always returns the hash of the child, unless the parent does not exist, in
-    /// which case it will return None.
-    fn insert(&'db self, parent: Hash, content: impl Iterator<Item=u8>) -> Option<Hash>;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
 
-    fn path(&'db self, hash: Hash) -> Option<Vec<Hash>>;
-  }
 
-  pub(crate) trait RawMerkleMetadataStore<'db>: MerkleMetadataStore<'db> {
+pub trait MerkleTreePlatform {
 
-    /// Like `insert`, but forces the caller to compute the hash of the content directly.
-    /// This function can be dangerous: it does not check that the content's hash matches
-    /// the provided hash value. If this occurs, the tree will be in an invalid state.
-    fn insert_with_hash(&'db self, parent: Hash, content: impl Iterator<Item=u8>, hash: Hash) -> Option<Hash>;
-  }
+trait Node {
+    fn hash(&self) -> Hash;
+    fn name(&self) -> &str;
+}
+type N : Node;
 
-  Ok(())
+trait Store {}
+type S : Store;
+
+}
+
+pub trait MerkleMetadataStore<'db> {
+
+/// If true, then there is a node in the Merkle tree that has this hash.
+fn exists(&'db self, hash: Hash) -> bool;
+
+/// Obtains a reference to the Merkle tree node for the given hash.
+/// None means there is no node with that hash.
+fn node(&'db self, hash: Hash) -> Option<&'db MerkleNode>;
+
+/// Ensures that `content` is a child of `parent`.
+/// If content is already a child of parent, then this does not change the Merkle tree.
+/// It always returns the hash of the child, unless the parent does not exist, in
+/// which case it will return None.
+fn insert(&'db self, parent: Hash, content: impl Iterator<Item=u8>) -> Option<Hash>;
+
+fn path(&'db self, hash: Hash) -> Option<Vec<Hash>>;
+}
+
+pub(crate) trait RawMerkleMetadataStore<'db>: MerkleMetadataStore<'db> {
+
+/// Like `insert`, but forces the caller to compute the hash of the content directly.
+/// This function can be dangerous: it does not check that the content's hash matches
+/// the provided hash value. If this occurs, the tree will be in an invalid state.
+fn insert_with_hash(&'db self, parent: Hash, content: impl Iterator<Item=u8>, hash: Hash) -> Option<Hash>;
 }
