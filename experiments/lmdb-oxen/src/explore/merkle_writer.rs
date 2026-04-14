53f4cfbe46437797d4138127ccd7392c1cd28bad
Malcolm Greaves <malcolm.g@oxen.ai>
2026-04-14 15:25:47 -0700

    async_trait for write and commit

--- experiments/lmdb-oxen/src/explore/merkle_writer.rs
+++ experiments/lmdb-oxen/src/explore/merkle_writer.rs
@@ -1,28 +1,35 @@
 use std::io;
 
+use async_trait::async_trait;
+
+#[async_trait]
 trait Transaction {
-    type Node;
+    type Node: Send + Sync + 'static;
 
     /// Queue the node for writing in the transaction.
     fn queue_write<'a>(&'a mut self, node: &'a Self::Node);
 
     /// Commit the transaction, writing all queued nodes.
-    fn commit(self) -> io::Result<()>;
+    async fn commit(self) -> io::Result<()>;
 }
 
+#[async_trait]
 trait MerkleWriter: Sized {
-    type T: Transaction;
+    type T: Transaction + Send + Sync + 'static;
 
     /// Open a write transaction for storing Merkle tree nodes.
     /// Allows multiple nodes to be queued for writing.
     fn write_transaction(&self) -> Self::T;
 
     /// Durably store a batch of Merkle tree nodes.
-    fn write(&self, nodes: impl Iterator<Item = <Self::T as Transaction>::Node>) -> io::Result<()> {
+    async fn write(
+        &self,
+        nodes: impl Iterator<Item = <Self::T as Transaction>::Node> + Send,
+    ) -> io::Result<()> {
         let mut tx = self.write_transaction();
         for node in nodes {
             tx.queue_write(&node);
         }
-        tx.commit()
+        tx.commit().await
     }
 }
